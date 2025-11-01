8a489715401968ccb4865d6b9cb94d43d04af667
Ruben <rubensloot20@outlook.com>
2025-11-01 20:32:07 +0100

    Add favicons

--- App.r
+++ App.r
@@ -562,6 +562,11 @@ ui <- fluidPage(
   
   div(id = "app",
       tags$head(
+          tags$link(rel = "icon", type = "image/x-icon", href = "img/favicon.ico"),
+          tags$link(rel = "icon", type = "image/png", sizes = "16x16", href = "img/favicon-16x16.png"),
+          tags$link(rel = "icon", type = "image/png", sizes = "32x32", href = "img/favicon-32x32.png"),
+          tags$link(rel = "apple-touch-icon", sizes = "180x180", href = "img/apple-touch-icon.png"),
+          tags$link(rel = "manifest", href = "img/site.webmanifest"),
           tags$script(src = "/colResizable-1.6.js"),
           tags$link(rel = "stylesheet", href = "appCSS.css"),
           tags$link(rel = "stylesheet", href = "select2.min.css"),
